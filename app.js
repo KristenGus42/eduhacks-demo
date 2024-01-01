cba37366f2dab63f72a99bd68d4392aacea645e6
Helena Kaikkonen <helenak@uw.edu>
2024-01-01 01:42:23 -0800

    Display chat history

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
-import express from 'express';
+import express, { response } from 'express';
 import { chat } from './scripts/openai-test.js';
 const app = express();
 
 app.get('/testing', async (req, res) => {
-  res.type('text');
-
   const userInput = req.query.message;
 
   if (userInput) {
     const chatRes = await chat(userInput);
-    res.send(chatRes.message.content);
+    let chatHTML = "";
+    for (let i = 0; i < chatRes.length; i++) {
+      if (chatRes[i].role == "user") {
+        chatHTML += "<p><strong>" + chatRes[i].content + "</strong></p>";
+      } else {
+        chatHTML += "<p>" + chatRes[i].content + "</p>";
+      }
+    }
+    res.send(chatHTML);
   } else {
     res.status(400).send('missing userInput param')
   }
 })
 
-app.listen(3000)
+app.listen(3000);
--- scripts/openai-test.js
+++ scripts/openai-test.js
@@ -2,13 +2,19 @@ import OpenAI from "openai";
 
 const openai = new OpenAI();
 
+const messages = [];
+
 export async function chat(query) {
+  messages.push({ role: "user", content: query });
+
+  console.log("Waiting for response...");
   const completion = await openai.chat.completions.create({
-    messages: [{ role: "system", content: query }],
+    messages: messages,
     model: "gpt-3.5-turbo",
   });
 
-  console.log(completion.choices[0]);
+  console.log("Tokens used: " + completion.usage.total_tokens);
+  messages.push({ role: "assistant", content: completion.choices[0].message.content });
 
-  return completion.choices[0];
+  return messages;
 }
